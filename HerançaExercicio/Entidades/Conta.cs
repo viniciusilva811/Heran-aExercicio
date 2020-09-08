9728d2991563326698d846b80dbe71f82087ca60
Vinícius Silva <vinisilva45@live.com>
2020-09-08 10:22:33 -0300

    Adicionar arquivos de projeto.

---
+++ HerançaExercicio/Entidades/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerançaExercicio.Entidades
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Saque(double dinheiro)
+        {
+            Saldo -= dinheiro;
+        }
+
+        public void Deposito(double dinheiro)
+        {
+            Saldo += dinheiro;
+        }
+    }
+}
